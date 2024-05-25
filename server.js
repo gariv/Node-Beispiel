d9e2ab96b7399eb670c3f0bf5f91857da727618b
gariv <114827684+gariv@users.noreply.github.com>
2024-05-25 23:44:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOps Course! First automatic deploy :-)');
+  res.send('Hello DevOps Course! First automatic deploy :-) And deploying automatically again.... ');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
